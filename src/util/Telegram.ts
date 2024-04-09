c963da3a4283f7fc993159d028aa76f2821618ec
AdrianCrack23 <78657915+AdrianCrack23@users.noreply.github.com>
2024-04-09 23:25:22 +0200

    Added Telegram.ts
    Little lies - Stunning shows

---
+++ src/util/Telegram.ts
@@ -0,0 +1,32 @@
+import { loadConfig } from './Load'
+
+export async function sendTGMessage() {
+
+    var { currentPoints, collectedPoints, currentMail, earnablePoints} = require('../index.js')
+    
+    const telegram = loadConfig().telegram
+
+    if (!telegram.enabled) return
+
+    let tg = {
+        token: telegram.token,
+        chat_id: telegram.chatid
+    }
+
+    const url = `https://api.telegram.org/bot${tg.token}/sendMessage`
+
+    var finalPoints=currentPoints+collectedPoints
+
+    const obj = {
+        chat_id: tg.chat_id,
+        text: "👤 Account: "+currentMail+"\n✨ Earnable Points: "+earnablePoints+"\n⭐️ Points earned today: "+collectedPoints+"\n💰 Total points: "+finalPoints
+    };
+
+    await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(obj)
+    });
+}
